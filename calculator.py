6f859d2d8701c213a0b39e8084918f8d92ea997e
nnagibator210-gif <nnagibator210@gmail.com>
2026-09-15 11:20:23 +0500

    Добавил вычитание и тесты

--- calculator.py
+++ calculator.py
@@ -1,14 +1,17 @@
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
 
-def discount_price(price, percent):
-    if price < 0 or percent < 0 or percent > 100:
-        raise ValueError("Некорректные данные")
-    return price * (1 - percent / 100)
+# def discount_price(price, percent):
+#     if price < 0 or percent < 0 or percent > 100:
+#         raise ValueError("Некорректные данные")
+#     return price * (1 - percent / 100)
 
-def is_adult(age):
-    return age >= 18
+# def is_adult(age):
+#     return age >= 18
 
 def multiply(a, b):
     return a * b
+
+def subtract(a, b):
+    return a - b
--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,17 @@
 # import pytest
 
 # from calculator import add, discount_price, is_adult
-from calculator import multiply
+from calculator import multiply, subtract
 
 def test_multiply():
     assert multiply(2, 3) == 6
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_subtract_negative():
+    assert subtract(3, 5) == -2
 
 
 # @pytest.mark.parametrize(
